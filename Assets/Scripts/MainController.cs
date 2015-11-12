f76d25ca1e4e4f35beab60a7c4f4e27943afbbcb
rgoomes <ricardo.jpg@hotmail.com>
2015-11-12 19:52:38 +0000

    New open options animation (its faster now), implemented circle swipe to rotate objects and scale swipe. Clean up code

--- Assets/Scripts/MainController.cs
+++ Assets/Scripts/MainController.cs
@@ -10,23 +10,20 @@ public class MainController : MonoBehaviour {
 	StateController sc; /* for menus */
 	Controller ct; /* to control leapmotion */
 
-    // This can be later used if leapmotion is buggy
-    /*
-	const int MIN_FREQ = 5;
-	int horzFreq;
-	int vertFreq;
-	*/
+	const int LFPS = 30; /* leapmotion frame every x game frames */
+	Frame lastFrame; /* the frame to detect motion */
+	int frameCounter; /* counter from 0 to fps */
 	
 	void Start(){
 		ct = new Controller();
 		ct.EnableGesture(Gesture.GestureType.TYPESWIPE);
-		//ct.Config.SetFloat("Gesture.Swipe.MinLength", 200.0f);
-		//ct.Config.SetFloat("Gesture.Swipe.MinVelocity", 100.0f);
+		ct.EnableGesture(Gesture.GestureType.TYPECIRCLE);
 		ct.Config.Save();
 
 		sc = new StateController(controlPanel, optionsPanel, successPanel);
 
-		// horzFreq = vertFreq = 0;
+		lastFrame = null;
+		frameCounter = 0;
 	}
 
 	public StateController getStateController(){
@@ -61,51 +58,81 @@ public class MainController : MonoBehaviour {
 			if(g.Type == Gesture.GestureType.TYPESWIPE){
 				SwipeGesture Swipe = new SwipeGesture(g);
 
-                bool isHorizontal = Mathf.Abs(Swipe.Direction.x) > Mathf.Abs(Swipe.Direction.y);
-                if (isHorizontal){
-                    if (Swipe.Direction.x > 0)
-                        rightSwipe();
-                    else
-                        leftSwipe();
-                }
-                else{
-                    if (Swipe.Direction.y > 0)
-                        upSwipe();
-                    else
-                        downSwipe();
-                }
-            }
+				bool isHorizontal = Mathf.Abs(Swipe.Direction.x) > Mathf.Abs(Swipe.Direction.y);
+				if (isHorizontal){
+					if (Swipe.Direction.x > 0)
+						rightSwipe();
+					else
+						leftSwipe();
+				}
+				else{
+					if (Swipe.Direction.y > 0)
+						upSwipe();
+					else
+						downSwipe();
+				}
+			} else if(g.Type == Gesture.GestureType.TYPECIRCLE){
+				// TODO: Rotate object with circular finger movement
+			}
 		}
 	}
-	
-	void Update(){
-		// Listen do leapmotion gestures
-		listenGestures();
 
-		// Test show/hide options. TODO: REMOVE THIS LATER
+	void detectMotion(){
+		if(!isTick())
+			return;
+
+		Frame frame = ct.Frame();
+		HandList hands = frame.Hands;
+
+		/* scale gesture requires two hands */
+		if(hands.Count != 2)
+			return;
+
+		float scaleFactor = hands[0].ScaleFactor(lastFrame)+hands[1].ScaleFactor(lastFrame);
+		scaleFactor /= 2; /* use average scale factor between both hands */
+
+		const float minScaleFactor = 0.25f;
+		if(Mathf.Abs(scaleFactor) > minScaleFactor){
+			// TODO: Apply rotation to selected object;
+		}
+
+	}
+
+	void kbInputTest(){
 		if(Input.GetKeyDown(KeyCode.LeftControl) == true )
 			upSwipe();
 		if(Input.GetKeyDown(KeyCode.RightControl) == true )
 			downSwipe();
-
-		// Test skip/enter tutorial. TODO: REMOVE THIS LATER
 		if(Input.GetKeyDown(KeyCode.LeftShift) == true )
 			leftSwipe();
 		if(Input.GetKeyDown(KeyCode.RightShift) == true )
 			rightSwipe();
-
 		if(Input.GetKeyDown(KeyCode.Space) == true )
 			sc.successTutorial();
+	}
 
-		// TODO: REMOVE THIS LATER, ONLY FOR PRESENTATION
-		if(sc.getPlace() == 6 && sc.canAnimate()){
-			this.transform.position = new Vector3(
-				-1700, transform.position.y, transform.position.z
-			);
-		} else if(sc.getPlace() == 0 && sc.canAnimate()){
-			this.transform.position = new Vector3(
-				-500, transform.position.y, transform.position.z
-			);
-		}
+	bool isTick(){
+		return frameCounter == 0;
+	}
+
+	void updateFrame(){
+		// TODO FIX: take into account frame delta
+		frameCounter = ++frameCounter % LFPS;
+
+		if(isTick())
+			lastFrame = ct.Frame();
+	}
+
+	void Update(){
+		// Update leapmotion frame
+		updateFrame();
+
+		// Listen do leapmotion gestures
+		listenGestures();
+		// Detect to leapmotion motion
+		detectMotion();
+
+		// Debug only
+		kbInputTest();
 	}
 }
